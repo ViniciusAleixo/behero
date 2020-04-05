2531ecd76be581a8c7c67d15d502deffb1d0a993
Administrator <administrator@milenas-iMac.hub>
2020-04-05 16:57:20 +0930

    Hero application

---
+++ mobile/src/pages/incidents/index.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from 'react';
+import { View, Image, Text, TouchableOpacity, FlatList } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import logoImg from '../../assets/logo.png';
+
+
+import styles from './styles';
+import api from '../../services/api';
+
+export default function Incidents() {
+const [incidents, setIncidents] = useState([]);
+const [total, setTotal] = useState(0);
+const [page, setPage] = useState(1);
+const [loading, setLoading] = useState(false);
+
+ const navigation = useNavigation();
+
+
+    function navigateToDetail(incident) {
+        navigation.navigate('Detail', { incident});
+    }
+
+    async function loadIncidents() {
+        if (loading) {
+            return;
+        }
+
+        if (total > 0 && incidents.length === total) {
+            return;
+        }
+
+        setLoading(true);
+        const res = await api.get('incidents', {
+            params: { page }
+        });
+        
+        setIncidents([...incidents, ...res.data]);
+        setTotal(res.headers['x-total-count']);
+        setPage(page + 1);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        loadIncidents();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logoImg} />
+                <Text style={styles.headerText}>
+                    Total cases: <Text style={styles.headerTextBold}>{total}</Text>
+                </Text>
+            </View>
+            <Text style={styles.title}>Welcome</Text>
+            <Text style={styles.description}>Choose an incident and be a Hero.</Text>
+
+            <FlatList 
+            data={incidents}
+            style={styles.incidentsList}
+            keyExtractor={incident => String(incident.id)}
+            showsVerticalScrollIndicator={false}
+            onEndReached={loadIncidents}
+            onEndReachedThreshold={0.2}
+            renderItem={({ item: incident }) => (
+                <View style={styles.incident}>
+                    <Text style={styles.incidentProperty}>ONG:</Text>
+                    <Text style={styles.incidentValue}>{incident.name}</Text>
+
+                    <Text style={styles.incidentProperty}>Case:</Text>
+                    <Text style={styles.incidentValue}>{incident.title}</Text>
+
+                    <Text style={styles.incidentProperty}>Amount</Text>
+                    <Text style={styles.incidentValue}>
+                        {Intl.NumberFormat('en-US', {
+                        style: 'currency',
+                         currency:'AUD'}).format(incident.value)}</Text>
+
+                    <TouchableOpacity 
+                        style={styles.detailButton}
+                        onPress={() => navigateToDetail(incident)}
+                    >
+                    <Text style={styles.detailsButtonText}>More details</Text>
+                    <Feather name="arrow-right" size={16} color="#E02041" />
+                    </TouchableOpacity>
+                </View>
+            )}
+            />
+        </View>
+      
+    );
+}
